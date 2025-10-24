df7ffe160bf57f3451219824ae0dd47a537799f6
Iván Vega García <pkp23714@educastur.es>
2025-10-24 12:13:31 +0200

    Inicio de ejercicio de printear nombres

--- PracticaGit_Equipo_Lucas_Ivan_Gillermo/src/ProjectoEntornosDesarrollo/Aplicacion.java
+++ PracticaGit_Equipo_Lucas_Ivan_Gillermo/src/ProjectoEntornosDesarrollo/Aplicacion.java
@@ -2,4 +2,11 @@ package ProjectoEntornosDesarrollo;
 
 public class Aplicacion {
 
+	public Aplicacion() {
+		
+	}
+	
+	public static void main(String args[]) {
+		Aplicacion aplicacion = new Aplicacion();
+	}
 }
