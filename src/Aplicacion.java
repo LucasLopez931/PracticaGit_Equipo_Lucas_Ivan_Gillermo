113b587acf08b9f243c1a0e2f8eb82d656325858
guillerdrr98 <guillerdrr98@educastur.es>
2025-10-29 12:51:52 +0100

    Se ha modificado el main

--- src/Aplicacion.java
+++ src/Aplicacion.java
@@ -5,9 +5,11 @@ public class Aplicacion {
 	public Aplicacion() {
 		Alumno ivan = new Ivan();
      	Alumno lucas1 =new Lucas();
+     	Alumno guillermo = new Guillermo();
      	
 		lucas1.saludar();
 		ivan.saludar();
+		guillermo.saludar();
 		
 	}
 
