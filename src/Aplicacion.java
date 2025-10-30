bd8b8023324fedb5bfdd0674885c6ce7f9412af5
Iván Vega García <pkp23714@educastur.es>
2025-10-30 08:36:03 +0100

    Creada linea de codigo para hacer la solucion de conflicto

--- src/Aplicacion.java
+++ src/Aplicacion.java
@@ -10,7 +10,7 @@ public class Aplicacion {
 		lucas1.saludar();
 		ivan.saludar();
 		guillermo.saludar();
-		
+		System.out.println("Hola voy a causar un conflicto de merges");
 	}
 
 	public static void main(String args[]) {
