5c91c8c2cd947c8d43f98f0a9cc82b1cb63f5d64
Iván Vega García <pkp23714@educastur.es>
2025-10-31 11:57:53 +0100

    Cambios para forzar el conficto con lucas

--- src/Aplicacion.java
+++ src/Aplicacion.java
@@ -9,13 +9,13 @@ public class Aplicacion {
      	
 		lucas1.saludar();
 		guillermo.saludar();		
-		System.out.println("Este cambio lo ha hecho Iván");
-		System.out.println("Cambio para conflicto pero en mi rama");
-		System.out.println("Este cambio lo ha hecho Guillermo");
+		System.out.println("Conflicto");
+		ivan.saludar();
+		System.out.println("Conflicto");
 	}
 
 	public static void main(String args[]) {
-		Aplicacion app = new Aplicacion();
+		Aplicacion MyApplicacion = new Aplicacion();
 	}
 	
 }
